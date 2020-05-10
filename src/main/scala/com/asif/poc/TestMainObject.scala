76849e4e150be088b580dbced8699d323afe40d6
asiftech4 <37262311+asiftech4@users.noreply.github.com>
2020-05-10 12:30:37 +0530

    Edited from github browser in develop

--- src/main/scala/com/asif/poc/TestMainObject.scala
+++ src/main/scala/com/asif/poc/TestMainObject.scala
@@ -3,6 +3,7 @@ package com.asif.poc
 object TestMainObject {
 
   def main(args: Array[String]): Unit = {
+    //Edited from github browser
     println("This is git test ")
     println("New feature added in develop")
   }

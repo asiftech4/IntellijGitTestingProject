1e72db938d365a0d2cf87ba8fe2e468b64115e5b
Asif Ali <asif.1.ali.ext@nokia.com>
2020-05-10 11:53:43 +0530

    Project Created initial setup

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "IntellijGitTestingProject"
+
+version := "0.1"
+
+scalaVersion := "2.11.11"
---
+++ src/main/scala/com/asif/poc/TestMainObject.scala
@@ -0,0 +1,8 @@
+package com.asif.poc
+
+object TestMainObject {
+
+  def main(args: Array[String]): Unit = {
+    println("This is git test ")
+  }
+}
